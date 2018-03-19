4016c6a445a4d5f8dfcc4a7a2f7f7004dd572c9e
JadonF <jadon.fournier@ocsbstudent.ca>
2018-03-19 10:15:38 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ChocolateBoxes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAsk = new System.Windows.Forms.Label();
+            this.nudInput = new System.Windows.Forms.NumericUpDown();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAsk
+            // 
+            this.lblAsk.AutoSize = true;
+            this.lblAsk.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblAsk.Location = new System.Drawing.Point(32, 40);
+            this.lblAsk.Name = "lblAsk";
+            this.lblAsk.Size = new System.Drawing.Size(435, 26);
+            this.lblAsk.TabIndex = 0;
+            this.lblAsk.Text = "Select how many chocolate boxes you sold.";
+            // 
+            // nudInput
+            // 
+            this.nudInput.Location = new System.Drawing.Point(179, 89);
+            this.nudInput.Name = "nudInput";
+            this.nudInput.Size = new System.Drawing.Size(120, 20);
+            this.nudInput.TabIndex = 1;
+            this.nudInput.ValueChanged += new System.EventHandler(this.nudInput_ValueChanged);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblAnswer.Location = new System.Drawing.Point(37, 189);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(85, 26);
+            this.lblAnswer.TabIndex = 2;
+            this.lblAnswer.Text = "Answer";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnCheck.Location = new System.Drawing.Point(199, 127);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 32);
+            this.btnCheck.TabIndex = 3;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 261);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.nudInput);
+            this.Controls.Add(this.lblAsk);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAsk;
+        private System.Windows.Forms.NumericUpDown nudInput;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Created by: Jadon Fournier
+ * Created on: 19-Mar-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #19 - Chocolate Boxes
+ * This program allows you to enter the number of boxes you sold, and tells you the prize you get.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChocolateBoxes
+{
+    public partial class Form1 : Form
+    {
+        //Set variables.
+        int numBoxes;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.lblAnswer.Hide();
+        }
+
+        private void nudInput_ValueChanged(object sender, EventArgs e)
+        {
+            numBoxes = (int)nudInput.Value;
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            if (numBoxes > 9)
+            {
+                if (numBoxes > 19)
+                {
+                    lblAnswer.Text = "You get a prize!";
+                }
+
+                else
+                {
+                    lblAnswer.Text = "You get a small prize.";
+                }
+            }
+
+            else
+            {
+                lblAnswer.Text = "You get an honourable mention.";
+            }
+            this.lblAnswer.Show();
+        }
+    }
+}
